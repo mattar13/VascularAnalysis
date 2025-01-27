855b46f5ca22f74f308ad5cfb9b88cb44af94210
mattar13 <mtarchick13@gmail.com>
2025-01-27 12:12:28 -0800

    update .gitignore and test

--- test.py
+++ test.py
@@ -2,4 +2,15 @@ from make_datafile import DataManager
 
 def main():
     "Testing main functions"
-    
+
+    #This is a test for loading data using the manager
+    #load_test = "test_files\\MasterSheet.xlsx"
+    #data_loaded = DataManager(load_test)
+
+    construct_test = "test_files\\FullLeaf_LengthByDistance_REMOVE_BLUE.xlsx"
+    data_constructed = DataManager()
+    data_constructed.construct_master_sheet_df(construct_test)
+    print(data_constructed.id_sheet_full)
+
+if __name__ == "__main__":
+    main()
